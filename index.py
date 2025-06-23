6cf2cc6c823014823beb641dc1e1a0107f99e64a
RanaG-851 <rana4801660@gmail.com>
2025-06-24 01:58:32 +0500

    Fix: bind Flask app to 0.0.0.0 and use Render port

--- index.py
+++ index.py
@@ -186,4 +186,7 @@ def upload_lab_report():
 
 if __name__ == "__main__":
     app.secret_key = os.urandom(24)
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
+
+    # app.run(debug=True)
